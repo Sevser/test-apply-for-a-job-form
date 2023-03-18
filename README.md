6f9d67bfa1407ee628af9f8310098a4860bd49ff
sevser <sevser39@gmail.com>
2023-03-18 15:01:55 +0400

    fix css

--- README.md
+++ README.md
@@ -1,15 +1,5 @@
 # test-apply-for-a-job-form
 
-This project is exists for showing possibility for saving all form logic inside store.
+This is educational project to obtain an experience with making forms with validation in store/components.
 
-apply for a job form should have follow fields:
-
-- name
-- last name
-- email
-- phone
-- applied position
-- earliest possible start date
-- preferred interview date
-- cover letter
-- CV
+Apply for a job form contains several fields. They some of they had a difficult validation, some of them have a simple validation.
--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import ApplyForm from './components/form/ApplyForm.vue';
 </script>
 
 <template>
-  <v-app>
+  <v-app full-height>
     <v-app-bar>
       <v-app-bar-title>
         Apply for a Job form
@@ -12,40 +12,14 @@ import ApplyForm from './components/form/ApplyForm.vue';
 
       <v-spacer></v-spacer>
     </v-app-bar>
-
-    <v-main class="mx-auto" style="width: max-content;">
-      <v-container class="overflow-y-auto">
-        <ApplyForm />
-      </v-container>
-    </v-main>
+    <v-container class="overflow-y-auto app-container">
+      <ApplyForm />
+    </v-container>
   </v-app>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.app-container {
+  width: 100%;
 }
 </style>
